76a34c9216873d426fb96d73cc03f9b51c345e64
Kaloian Ianev <kianev@gmail.com>
2018-04-21 16:26:07 +0300

    Contact Form created

--- portfolio/src/app/contact/contact.component.ts
+++ portfolio/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-contact',
@@ -12,4 +13,8 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
+    form.resetForm();
+  }
 }
